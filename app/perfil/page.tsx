3600d344769acdb8e2dabec51a253a682f476b00
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2024-01-12 10:10:51 -0300

    fix: estilo seccion perfil

--- app/perfil/page.tsx
+++ app/perfil/page.tsx
@@ -21,7 +21,7 @@ const styles = {
     width: "600px",
     maxHeight: "600px"
   },
-  grid: { maxWidth: "500px", height: "600px" },
+  grid: { padding: "0px 80px", height: "600px" },
   item: { display: "flex", alignItems: "center" },
   avatar: {
     bgcolor: grey[500],
@@ -31,7 +31,7 @@ const styles = {
   containerButton: {
     display: "flex",
     alignItems: "center",
-    justifyContent: "top"
+    justifyContent: "right"
   },
   deleteButton: { marginTop: "15px" },
   itemCuenta: { display: "flex", alignItems: "center", flexWrap: "wrap", borderRadius: "5px" }
@@ -64,82 +64,91 @@ const Page = () => {
   return (
     <Container sx={styles.container}>
       <Box sx={styles.box}>
-        <Grid container sx={styles.grid} rowSpacing={{ xs: 1, sm: 2, md: 2 }}>
+        <Grid container sx={styles.container} rowSpacing={3}>
 
           <Grid item xs={12}>
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              aria-label="basic tabs example"
-              textColor="inherit"
-              indicatorColor="primary">
-              <Tab label="Perfil" {...a11yProps(0)} />
-              <Tab label="Seguridad" {...a11yProps(1)} />
-              <Tab label="Cuenta" {...a11yProps(2)} />
-            </Tabs>
+            <Box sx={styles.container}>
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                aria-label="basic tabs example"
+                textColor="inherit"
+                indicatorColor="primary">
+                <Tab label="Perfil" {...a11yProps(0)} />
+                <Tab label="Seguridad" {...a11yProps(1)} />
+                <Tab label="Cuenta" {...a11yProps(2)} />
+              </Tabs>
+            </Box>
           </Grid>
 
-          {
-            value === 0 &&
-            <Grid item xs={12}>
-              <Box hidden={value !== 0} >
-                <Grid container rowSpacing={{ xs: 1, sm: 2, md: 6 }}>
-                  <Grid item xs={6} >
-                    <Avatar sx={styles.avatar} alt="Claudio Bustos" src="img/perfil.jpg" />
-                  </Grid>
-                  <Grid item xs={6} sx={styles.containerButton}>
-                    <Tooltip title="Editar perfil">
-                      <Button startIcon={<CreateIcon />} color="primary" />
-                    </Tooltip>
-                  </Grid>
-                  {
-                    items.map(({ label, text }, key) => (
-                      <Grid sx={styles.item} item xs={label === "Correo Electrónico" ? 12 : 6} key={key}>
-                        <LabelCustom primary={text} secondary={label} />
+          <Grid item xs={12}>
+            <Grid container sx={styles.grid} rowSpacing={{ xs: 1, sm: 1, md: 1 }}>
+              {
+                value === 0 &&
+                <Grid item xs={12}>
+                  <Box hidden={value !== 0} >
+                    <Grid container rowSpacing={{ xs: 1, sm: 2, md: 4 }}>
+                      <Grid item xs={12}>
+                        <Box sx={styles.containerButton}>
+                          <Tooltip title="Editar perfil">
+                            <Button startIcon={<CreateIcon />} color="primary" />
+                          </Tooltip>
+                        </Box>
                       </Grid>
-                    ))
-                  }
+                      <Grid item xs={12} >
+                        <Avatar sx={styles.avatar} alt="Claudio Bustos" src="img/perfil.jpg" />
+                      </Grid>
+                      {
+                        items.map(({ label, text }, key) => (
+                          <Grid sx={styles.item} item xs={label === "Correo Electrónico" ? 12 : 6} key={key}>
+                            <LabelCustom primary={text} secondary={label} />
+                          </Grid>
+                        ))
+                      }
+                    </Grid>
+                  </Box>
                 </Grid>
-              </Box>
-            </Grid>
-          }
+              }
 
-          {
-            value === 1 &&
-            <Grid item xs={12}>
-              <Box hidden={value !== 1}>
-                <Grid container>
-                  <Grid sx={styles.item} item xs={6}>
-                    <LabelCustom primary={"****************"} secondary={"Contraseña"} />
-                  </Grid>
-                  <Grid sx={styles.item} item xs={6}>
-                    <TextField color="success" variant="standard" label="Nueva Contraseña" autoComplete="off" />
-                  </Grid>
+              {
+                value === 1 &&
+                <Grid item xs={12}>
+                  <Box hidden={value !== 1}>
+                    <Grid container>
+                      <Grid sx={styles.item} item xs={6}>
+                        <LabelCustom primary={"****************"} secondary={"Contraseña"} />
+                      </Grid>
+                      <Grid sx={styles.item} item xs={6}>
+                        <TextField color="success" variant="standard" label="Nueva Contraseña" autoComplete="off" />
+                      </Grid>
+                    </Grid>
+                  </Box>
                 </Grid>
-              </Box>
-            </Grid>
-          }
+              }
 
-          {
-            value === 2 &&
-            <Grid item xs={12}>
-              <Box hidden={value !== 2}>
-                <Grid item xs={12} sx={styles.itemCuenta}>
-                  <Typography color="error" fontSize={20}>
-                    Dar de baja mi Cuenta
-                  </Typography>
-                  <Typography fontSize={15} color="error">
-                    Al dar de baja su cuenta la misma estará disponible
-                    durante 30 días por si se arrepiente de su decision, luego de pasado los
-                    30 días su cuenta será eliminada permanentemente.
-                  </Typography>
-                  <Button sx={styles.deleteButton} variant="outlined" startIcon={<DeleteIcon />} color="error" >
-                    Eliminar cuenta
-                  </Button>
+              {
+                value === 2 &&
+                <Grid item xs={12}>
+                  <Box hidden={value !== 2}>
+                    <Grid item xs={12} sx={styles.itemCuenta}>
+                      <Typography color="error" fontSize={20}>
+                        Dar de baja mi Cuenta
+                      </Typography>
+                      <Typography fontSize={15} color="error">
+                        Al dar de baja su cuenta la misma estará disponible
+                        durante 30 días por si se arrepiente de su decision, luego de pasado los
+                        30 días su cuenta será eliminada permanentemente.
+                      </Typography>
+                      <Button sx={styles.deleteButton} variant="outlined" startIcon={<DeleteIcon />} color="error" >
+                        Eliminar cuenta
+                      </Button>
+                    </Grid>
+                  </Box>
                 </Grid>
-              </Box>
+              }
+
             </Grid>
-          }
+          </Grid>
 
         </Grid>
       </Box>
