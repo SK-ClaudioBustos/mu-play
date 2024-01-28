dcc3457a443946269214468cd9cc6a0d77423a53
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2024-01-28 15:49:23 -0300

    style: menu settings

--- app/theme.ts
+++ app/theme.ts
@@ -28,7 +28,8 @@ const theme = createTheme({
         styleOverrides: {
           paper: {
             boxShadow: "0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)",
-            backgroundColor: "#CCFFEB"
+            backgroundColor: "#54e8b0",
+            borderRadius: "0px"
           }
         }
       }
