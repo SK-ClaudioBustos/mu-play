85ba6e76b0ec96b13aa6b1fda3fc9cea02e0fae3
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2024-08-08 14:21:03 -0300

    feat: boton de reproducir

--- app/ui/components/ContentItem.tsx
+++ app/ui/components/ContentItem.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { useMediaQuery } from "@mui/material"
+import PlayArrowIcon from '@mui/icons-material/PlayArrow'
+import { IconButton, Tooltip, useMediaQuery } from "@mui/material"
 import Box from "@mui/material/Box"
 import Card from "@mui/material/Card"
 import CardContent from "@mui/material/CardContent"
@@ -8,8 +9,8 @@ import Grid from "@mui/material/Grid"
 import Image from 'next/image'
 import { ContentData } from "../data/data_sections"
 import LabelCustom from "../utils/LabelCustom"
-import ciStyle from "../utils/styles/contentItem"
 import { color2 } from "../utils/styles/colors"
+import ciStyle from "../utils/styles/contentItem"
 
 const ContentItem = (props: ContentData) => {
     const {
@@ -22,25 +23,36 @@ const ContentItem = (props: ContentData) => {
     return (
         <Grid item md={6} sm={12} xs={12}>
             <Card sx={ciStyle.card}>
-                <Image
-                    priority
-                    width={matches ? 90 : 150}
-                    height={matches ? 90 : 150}
-                    alt="imagen de album"
-                    src={image}
-                />
-                <Box sx={ciStyle.box}>
-                    <CardContent sx={ciStyle.content}>
-                        <LabelCustom
-                            primary={song || "---"}
-                            secondary={artist || "---"}
-                            colorP={color2}
-                            colorS="#1E9467"
-                            fs1={matches ? "3.8vw" : "1.5rem"}
-                            fs2={matches ? "3.3vw" : "1.5rem"}
+                <Grid container>
+                    <Grid item sm={10} xs={10} sx={{ display: "flex", flexDirection: "row"}}>
+                        <Image
+                            priority
+                            width={matches ? 90 : 150}
+                            height={matches ? 90 : 150}
+                            alt="imagen de album"
+                            src={image}
                         />
-                    </CardContent>
-                </Box>
+                        <Box sx={ciStyle.box}>
+                            <CardContent sx={ciStyle.content}>
+                                <LabelCustom
+                                    primary={song || "---"}
+                                    secondary={artist || "---"}
+                                    colorP={color2}
+                                    colorS="#1E9467"
+                                    fs1={matches ? "3.8vw" : "1.5rem"}
+                                    fs2={matches ? "3.3vw" : "1.5rem"}
+                                />
+                            </CardContent>
+                        </Box>
+                    </Grid>
+                    <Grid item sm={2} xs={2} sx={{ display: "flex", alignItems: "center"}}>
+                        <IconButton>
+                            <Tooltip title="Reproducir">
+                                <PlayArrowIcon color="action" sx={{ fontSize: matches ? "4vw" : "1.6rem" }} />
+                            </Tooltip>
+                        </IconButton>
+                    </Grid>
+                </Grid>
             </Card>
         </Grid>
     )
