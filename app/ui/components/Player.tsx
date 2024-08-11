b58a5328d29d386850300fce9c0338a3343e0540
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2024-08-11 18:43:48 -0300

    refactor: responsive player

--- app/ui/components/Player.tsx
+++ app/ui/components/Player.tsx
@@ -7,25 +7,17 @@ import PauseRounded from '@mui/icons-material/PauseRounded';
 import PlayArrowRounded from '@mui/icons-material/PlayArrowRounded';
 import VolumeDownRounded from '@mui/icons-material/VolumeDownRounded';
 import VolumeUpRounded from '@mui/icons-material/VolumeUpRounded';
-import { Grid } from '@mui/material';
+import { Grid, useMediaQuery } from '@mui/material';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import Slider from '@mui/material/Slider';
 import Stack from '@mui/material/Stack';
-import { styled, useTheme } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
 import { Dispatch, SetStateAction } from 'react';
 import { color5 } from '../utils/styles/colors';
-const Widget = styled('div')(({ theme }) => ({
-    padding: "0.9rem",
-    borderRadius: 10,
-    maxWidth: '80%',
-    margin: 'auto',
-    position: 'relative',
-    zIndex: 10,
-    backgroundColor: color5,
-}));
+
 
 const TinyText = styled(Typography)({
     fontSize: '0.75rem',
@@ -40,10 +32,19 @@ interface Props {
 
 export default function Player(props: Props) {
     const { songData, setShowPlayer } = props;
-    const theme = useTheme();
+    const media = useMediaQuery('(max-width:600px)');
     const [paused, setPaused] = React.useState(false);
+    const Widget = styled('div')(({ theme }) => ({
+        padding: "0.9rem",
+        borderRadius: media ? 0 : 10,
+        maxWidth: media ? "100%" : '80%',
+        margin: 'auto',
+        position: 'relative',
+        zIndex: 10,
+        backgroundColor: color5,
+    }));
     return (
-        <Box sx={{ width: '100%', position: "fixed", bottom: "0", marginBottom: "5px" }}>
+        <Box sx={{ width: '100%', position: "fixed", bottom: "0", marginBottom: media ? "0px" : "5px" }}>
             <Widget>
                 <Grid container>
                     <Grid item sm={12} xs={12} display={"flex"} justifyContent={"space-between"}>
@@ -54,35 +55,41 @@ export default function Player(props: Props) {
                                 </Typography>
                             </Box>
                         </Box>
-                        <Box width={"20%"} display={"flex"} justifyContent={"space-between"}>
-                            <Stack flexBasis={"100%"} spacing={2} direction="row" sx={{ px: 1 }} alignItems="center">
-                                <VolumeDownRounded fontSize='small' />
-                                <Slider
-                                    aria-label="volumen"
-                                    defaultValue={30}
-                                    sx={{
-                                        color: theme.palette.mode === 'dark' ? '#fff' : 'rgba(0,0,0,0.87)',
-                                        '& .MuiSlider-track': {
-                                            border: 'none',
-                                        },
-                                        '& .MuiSlider-thumb': {
-                                            width: 15,
-                                            height: 15,
-                                            backgroundColor: '#fff',
-                                            '&::before': {
-                                                boxShadow: '0 4px 8px rgba(0,0,0,0.4)',
-                                            },
-                                            '&:hover, &.Mui-focusVisible, &.Mui-active': {
-                                                boxShadow: 'none',
-                                            },
-                                        },
-                                    }}
-                                />
-                                <VolumeUpRounded fontSize='small' />
-                            </Stack>
-                            <IconButton sx={{ padding: "0px" }} aria-label="cerrar reproductor">
-                                <CloseIcon onClick={() => setShowPlayer(false)} />
-                            </IconButton>
+                        <Box width={"40%"} display={"flex"} justifyContent={"space-between"}>
+                            {
+                                !media && (
+                                    <>
+                                        <Stack flexBasis={"100%"} spacing={2} direction="row" sx={{ px: 1 }} alignItems="center">
+                                            <VolumeDownRounded fontSize='small' />
+                                            <Slider
+                                                aria-label="volumen"
+                                                defaultValue={30}
+                                                sx={{
+                                                    color: 'rgba(0,0,0,0.87)',
+                                                    '& .MuiSlider-track': {
+                                                        border: 'none',
+                                                    },
+                                                    '& .MuiSlider-thumb': {
+                                                        width: 15,
+                                                        height: 15,
+                                                        backgroundColor: '#fff',
+                                                        '&::before': {
+                                                            boxShadow: '0 4px 8px rgba(0,0,0,0.4)',
+                                                        },
+                                                        '&:hover, &.Mui-focusVisible, &.Mui-active': {
+                                                            boxShadow: 'none',
+                                                        },
+                                                    },
+                                                }}
+                                            />
+                                            <VolumeUpRounded fontSize='small' />
+                                        </Stack>
+                                        <IconButton onClick={() => setShowPlayer(false)} sx={{ padding: "0px" }} aria-label="cerrar reproductor">
+                                            <CloseIcon />
+                                        </IconButton>
+                                    </>
+                                )
+                            }
                         </Box>
                     </Grid>
                     <Grid item sm={12} xs={12} sx={{ display: "flex", alignItems: "center" }} paddingRight={"1rem"}>
@@ -95,7 +102,7 @@ export default function Player(props: Props) {
                                 step={1}
                                 max={200}
                                 sx={{
-                                    color: theme.palette.mode === 'dark' ? '#fff' : 'rgba(0,0,0,0.87)',
+                                    color: 'rgba(0,0,0,0.87)',
                                     paddingBottom: 0,
                                     '& .MuiSlider-thumb': {
                                         width: 8,
@@ -105,10 +112,7 @@ export default function Player(props: Props) {
                                             boxShadow: '0 2px 12px 0 rgba(0,0,0,0.4)',
                                         },
                                         '&:hover, &.Mui-focusVisible': {
-                                            boxShadow: `0px 0px 0px 8px ${theme.palette.mode === 'dark'
-                                                ? 'rgb(255 255 255 / 16%)'
-                                                : 'rgb(0 0 0 / 16%)'
-                                                }`,
+                                            boxShadow: `0px 0px 0px 8px rgb(0 0 0 / 16%)`,
                                         },
                                         '&.Mui-active': {
                                             width: 20,
